0e2f29edc8d2acfc8c0cebee1678c2f1534b8654
jamezchard <cookcooller@163.com>
2025-08-14 20:11:55 +0800

    a workable dape config

--- elisp/init-programming.el
+++ elisp/init-programming.el
@@ -103,6 +103,73 @@
   (add-to-list 'copilot-indentation-alist '(closure-mode 2))
   (add-to-list 'copilot-indentation-alist '(emacs-lisp-mode 2)))
 
+(use-package dape
+  :ensure t
+  :preface
+  ;; By default dape shares the same keybinding prefix as `gud'
+  ;; If you do not want to use any prefix, set it to nil.
+  ;; (setq dape-key-prefix "\C-x\C-a")
+
+  :hook
+  ;; Save breakpoints on quit
+  (kill-emacs . dape-breakpoint-save)
+  ;; Load breakpoints on startup
+  (after-init . dape-breakpoint-load)
+
+  :config
+  ;; Turn on global bindings for setting breakpoints with mouse
+  (dape-breakpoint-global-mode)
+
+  ;; Info buffers to the right
+  (setq dape-buffer-window-arrangement 'right)
+
+  ;; Info buffers like gud (gdb-mi)
+  (setq dape-buffer-window-arrangement 'gud)
+  (setq dape-info-hide-mode-line nil)
+
+  ;; Pulse source line (performance hit)
+  (add-hook 'dape-display-source-hook 'pulse-momentary-highlight-one-line)
+
+  ;; Showing inlay hints
+  (setq dape-inlay-hints t)
+
+  ;; Save buffers on startup, useful for interpreted languages
+  (add-hook 'dape-start-hook (lambda () (save-some-buffers t t)))
+
+  ;; Kill compile buffer on build success
+  (add-hook 'dape-compile-hook 'kill-buffer)
+
+  ;; Projectile users
+  (setq dape-cwd-function 'projectile-project-root))
+
+
+(with-eval-after-load 'dape
+  (add-to-list 'dape-configs
+               '(dbgpy
+                 modes (python-mode python-ts-mode)
+                 ensure (lambda (config)
+                          (dape-ensure-command config)
+                          (let ((python (dape-config-get config 'command)))
+                            (unless (zerop (call-process python nil nil nil "-c" "import debugpy.adapter"))
+                              (user-error "%s module debugpy is not installed" python))))
+                 command "D:/pyEnv/Scripts/python.exe"
+                 command-args ("-m" "debugpy.adapter" "--host" "127.0.0.1" "--port" "19999")
+                 port 19999
+                 :request "launch"
+                 :type "python"
+                 :cwd dape-cwd
+                 :program dape-buffer-default
+                 :args []
+                 :justMyCode nil
+                 :console "integratedTerminal"
+                 :showReturnValue t
+                 :stopOnEntry nil)))
+
+(use-package repeat
+  :config
+  (repeat-mode))
+
+
 (use-package pyvenv
   :ensure t
   :config
